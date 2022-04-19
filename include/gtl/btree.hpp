a0b7103173187c3ba44398e3c2c8b791d3acc1cd
greg <greg7mdp@gmail.com>
2022-04-19 07:38:02 -0400

    Add fix for phmap issue #138

--- include/gtl/btree.hpp
+++ include/gtl/btree.hpp
@@ -2395,11 +2395,13 @@ namespace priv {
             // ----------------------------------------------------------------------
             right->uninitialized_move_n(to_move, right->count() - to_move,
                                         right->count(), right, alloc);
-            for (slot_type *src = right->slot(right->count() - to_move - 1),
-                     *dest = right->slot(right->count() - 1),
-                     *end = right->slot(0);
-                 src >= end; --src, --dest) {
-                params_type::move(alloc, src, dest);
+            if (right->count() > to_move) {
+                for (slot_type *src = right->slot(right->count() - to_move - 1),
+                         *dest = right->slot(right->count() - 1),
+                         *end = right->slot(0);
+                     src >= end; --src, --dest) {
+                    params_type::move(alloc, src, dest);
+                }
             }
 
             // 2) Move the delimiting value in the parent to the right node.
