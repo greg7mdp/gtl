90f0a979efabc932484f3cbc51a0f75dfcfb3940
greg7mdp <greg7mdp@gmail.com>
2022-07-22 08:51:46 -0400

    Fix imperfect hash for tuple of small integers - better implementation of `gtl::HashState().combine(` from boost

---
+++ examples/hmap/hash.cpp
@@ -0,0 +1,38 @@
+#include <gtl/phmap_utils.hpp> // minimal header providing gtl::HashState()
+#include <string>
+#include <utility>
+#include <tuple>
+#include <vector>
+#include <array>
+//#include <string_view>
+#include <iostream>
+
+using std::string;
+using std::tuple;
+using std::pair;
+
+using groupid_t = std::array<uint16_t, 4>;
+
+namespace std
+{
+    template<> struct hash<groupid_t>
+    {
+        std::size_t operator()(groupid_t const &g) const
+        {
+            return gtl::Hash<decltype(std::tuple_cat(g))>()(std::tuple_cat(g));
+        }
+    };
+}
+
+int main()
+{
+    std::vector<groupid_t> groups = {
+        {17, 75, 82, 66},
+        {22, 88, 54, 42},
+        {11, 55, 77, 99} };
+
+    for (const auto &g : groups)
+        std::cout << std::hash<groupid_t>()(g) << '\n';
+    
+    return 0;
+}
--- include/gtl/phmap_utils.hpp
+++ include/gtl/phmap_utils.hpp
@@ -289,6 +289,13 @@ struct Hash<double> : public gtl_unary_function<double, size_t>
 
 #endif
 
+#if defined(_MSC_VER)
+#   define GTL_HASH_ROTL32(x, r) _rotl(x,r)
+#else
+#   define GTL_HASH_ROTL32(x, r) (x << r) | (x >> (32 - r))
+#endif
+
+
 template <class H, int sz> struct Combiner
 {
     H operator()(H seed, size_t value);
@@ -296,17 +303,48 @@ template <class H, int sz> struct Combiner
 
 template <class H> struct Combiner<H, 4>
 {
-    H operator()(H seed, size_t value)
+    H operator()(H h1, size_t k1)
     {
-        return seed ^ (value + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+        // Copyright 2005-2014 Daniel James.
+        // Distributed under the Boost Software License, Version 1.0. (See accompanying
+        // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+        const uint32_t c1 = 0xcc9e2d51;
+        const uint32_t c2 = 0x1b873593;
+
+        k1 *= c1;
+        k1 = GTL_HASH_ROTL32(k1,15);
+        k1 *= c2;
+
+        h1 ^= k1;
+        h1 = GTL_HASH_ROTL32(h1,13);
+        h1 = h1*5+0xe6546b64;
+
+        return h1;
     }
 };
 
 template <class H> struct Combiner<H, 8>
 {
-    H operator()(H seed, size_t value)
+    H operator()(H h, size_t k)
     {
-        return seed ^ (value + size_t(0xc6a4a7935bd1e995) + (seed << 6) + (seed >> 2));
+        // Copyright 2005-2014 Daniel James.
+        // Distributed under the Boost Software License, Version 1.0. (See accompanying
+        // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+        const uint64_t m = (uint64_t(0xc6a4a793) << 32) + 0x5bd1e995;
+        const int r = 47;
+
+        k *= m;
+        k ^= k >> r;
+        k *= m;
+
+        h ^= k;
+        h *= m;
+
+        // Completely arbitrary number, to prevent 0's
+        // from hashing to 0.
+        h += 0xe6546b64;
+
+        return h;
     }
 };
 
@@ -350,21 +388,24 @@ struct Hash<std::pair<T1, T2>> {
 template<class... T> 
 struct Hash<std::tuple<T...>> {
     size_t operator()(std::tuple<T...> const& t) const noexcept {
-        return _hash_helper(t);
+        size_t seed = 0;
+        return _hash_helper(seed, t);
     }
 
-private:
-    template<size_t I = 0, class ...P>
-        typename std::enable_if<I == sizeof...(P), size_t>::type
-        _hash_helper(const std::tuple<P...> &) const noexcept { return 0; }
 
-    template<size_t I = 0, class ...P>
-    typename std::enable_if<I < sizeof...(P), size_t>::type
-    _hash_helper(const std::tuple<P...> &t) const noexcept {
+
+private:
+    template<size_t I = 0, class TUP>
+    typename std::enable_if<I == std::tuple_size<TUP>::value, size_t>::type
+    _hash_helper(size_t seed, const TUP &) const noexcept { return seed; }
+
+    template<size_t I = 0, class TUP>
+    typename std::enable_if<I < std::tuple_size<TUP>::value, size_t>::type
+    _hash_helper(size_t seed, const TUP &t) const noexcept {
         const auto &el = std::get<I>(t);
         using el_type = typename std::remove_cv<typename std::remove_reference<decltype(el)>::type>::type;
-        return Combiner<size_t, sizeof(size_t)>()(
-            gtl::Hash<el_type>()(el),  _hash_helper<I + 1>(t));
+        seed = Combiner<size_t, sizeof(size_t)>()(seed, gtl::Hash<el_type>()(el));
+        return _hash_helper<I + 1>(seed, t);
     }
 };
 
