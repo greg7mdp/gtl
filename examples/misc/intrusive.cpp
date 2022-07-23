4f128c45f6423b6b0c8ab04bc5ae28bbb9a76882
greg7mdp <greg7mdp@gmail.com>
2022-07-23 13:13:13 -0400

    Add intrusive header and example

---
+++ examples/misc/intrusive.cpp
@@ -0,0 +1,40 @@
+#include <cstring>
+#include <gtl/intrusive.hpp>
+
+struct A : public gtl::intrusive_ref_counter<A, gtl::thread_safe_counter>
+{
+    virtual ~A() { std::cout << "A deleted" << std::endl; }
+    int x;
+};
+
+struct D : public A
+{
+    ~D() { std::cout << "D deleted" << std::endl; }
+    int y;
+};
+
+struct B : public gtl::intrusive_ref_counter<B, gtl::thread_unsafe_counter>
+{
+    ~B() { std::cout << "B deleted" << std::endl; }
+    int x;
+};
+
+
+
+
+int main()
+{
+    {
+        gtl::intrusive_ptr<A> a = new A;;
+
+        {
+            auto a2 = a;
+            gtl::intrusive_ptr<D> d = new D ;
+            {
+                gtl::intrusive_ptr<A> d2 = d ;
+            }
+        }
+    }
+
+     return 0;
+}
