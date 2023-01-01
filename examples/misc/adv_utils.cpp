75cdaedcc3c883b139f3976b86190d21bacc74b8
greg7mdp <greg7mdp@gmail.com>
2023-01-01 18:27:28 -0500

    Add implementation of binary_search using concepts
    inspired from https://byorgey.wordpress.com/2023/01/01/competitive-programming-in-haskell-better-binary-search/

---
+++ examples/misc/adv_utils.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <cstdio>
+#include <gtl/adv_utils.hpp>
+
+int main()
+{
+    {
+        // For example, we can use gtl::binary_search to take an integer square root:
+        auto x = gtl::binary_search(
+            gtl::middle<int>, [](int x) { return x * x >= 150; }, 0, 100);
+        printf("%d < integral sqrt(150) <= %d\n", x.first, x.second);
+    }
+
+    {
+        // Or floating points
+        auto x = gtl::binary_search(
+            gtl::middle_d<double>, [](double x) { return x * x >= 150.0; }, 0.0, 100.0);
+        printf("%.15f < float sqrt(150) <= %.15f\n", x.first, x.second);
+    }
+
+    return 0;
+}
---
+++ include/gtl/adv_utils.hpp
@@ -0,0 +1,91 @@
+#ifndef gtl_adv_utils_hpp_guard
+#define gtl_adv_utils_hpp_guard
+
+// ---------------------------------------------------------------------------
+// Copyright (c) 2023, Gregory Popovitch - greg7mdp@gmail.com
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+// ---------------------------------------------------------------------------
+
+#include <concepts>
+#include <cstdint>
+#include <optional>
+#include <utility>
+
+namespace gtl {
+
+// ---------------------------------------------------------------------------
+// Generalized binary search
+// inspired by the fun post from Brent Yorgey.
+// https://byorgey.wordpress.com/2023/01/01/competitive-programming-in-haskell-better-binary-search/
+// also see: https://julesjacobs.com/notes/binarysearch/binarysearch.pdf
+// ---------------------------------------------------------------------------
+
+// Concept for "Middle" function.
+// compute a potential next value to look at, given the current interval. When
+// mid l r returns a value, it must be strictly in between l and r
+// ---------------------------------------------------------------------------
+template<class T, class MiddleFn>
+concept MiddlePredicate = requires(MiddleFn fn, T l, T r) {
+    {
+        fn(l, r)
+    } -> std::same_as<std::optional<T>>;
+};
+
+// Concept for a Boolean predicate function
+//
+template<class T, class BoolPredFn>
+concept BooleanPredicate = requires(BoolPredFn fn, T val) {
+    {
+        fn(val)
+    } -> std::convertible_to<bool>;
+};
+
+// returns a pair<T, T> such that pred(first) == false and pred(second) == true
+// in the interval [l, r], pred should switch from false to true exactly once.
+template<class T, class Pred, class Middle>
+std::pair<T, T> binary_search(const Middle& middle, const Pred& pred, T l, T r)
+    requires MiddlePredicate<T, Middle> && BooleanPredicate<T, Pred>
+{
+    assert(pred(l) == false && pred(r) == true);
+    auto m = middle(l, r);
+    if (!m)
+        return { l, r };
+    return pred(*m) ? binary_search<T, Pred, Middle>(middle, pred, l, *m)
+                    : binary_search<T, Pred, Middle>(middle, pred, *m, r);
+}
+
+// ---------------------------------------------------------------------------
+// Some middle functions
+// ---------------------------------------------------------------------------
+
+// We stop when l and r are exactly one apart; otherwise we return their midpoint (you should
+// convince yourself that (l+r) / 2 is always strictly in between l and r when r - l > 1).
+// ------------------------------------------------------------------------------------------
+template<std::integral T>
+std::optional<T> middle(T l, T r)
+{
+    return r - l > 1 ? std::optional<T>{ (l + r) / 2 } : std::optional<T>{};
+}
+
+// Compare doubles using the binary representation
+// ------------------------------------------------------------------------------------------
+template<class T>
+std::optional<double> middle_d(double l, double r)
+{
+    uint64_t* il = (uint64_t*)&l;
+    uint64_t* ir = (uint64_t*)&r;
+    if (*ir - *il > 1) {
+        uint64_t m = (*il + *ir) / 2;
+        return std::optional<double>{ *(double*)&m };
+    }
+    return std::optional<double>{};
+}
+
+} // namespace gtl
+
+#endif // gtl_adv_utils_hpp_guard
