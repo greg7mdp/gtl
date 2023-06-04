4caee2d0ff0d5c160993e3ab2fc5f2c81b1eb9ae
greg7mdp <greg7mdp@gmail.com>
2023-06-04 11:10:23 -0400

    Add GTL_ASSUME macro (unused for now).

--- include/gtl/gtl_config.hpp
+++ include/gtl/gtl_config.hpp
@@ -233,6 +233,16 @@
     #define GTL_ATTRIBUTE_NO_UNIQUE_ADDRESS
 #endif
 
+#if GTL_HAVE_ATTRIBUTE(assume)
+    #define GTL_ASSUME(expr) [[assume(expr)]]
+#elif defined(__clang__)
+    #define GTL_ASSUME(expr) __builtin_assume(expr)
+#elif defined(_MSC_VER) || defined(__ICC))
+    #define GTL_ASSUME(expr) __assume(expr)
+#else
+    #define GTL_ASSUME(expr)
+#endif
+
 // ----------------------------------------------------------------------
 // Figure out SSE support
 // ----------------------------------------------------------------------
