b4ac266c9008b6a1ea3d92633adb2066c05a48e6
Gregory Popovitch <greg7mdp@gmail.com>
2026-01-08 20:10:52 -0500

    More fixes in `gtl.natvis`.  (#48)
    * Fix issues in `gtl.natvis`
    
    * Add `debuggervis.cpp` example.
    
    * Add commputed size for parallel map/set containets when possible.
    
    * Add max display of 100 elements for gtl::vector, and size display for btree containers.
    
    * Add debug vis for `bit_vector.hpp`.

---
+++ examples/misc/debuggervis.cpp
@@ -0,0 +1,72 @@
+#include <gtl/phmap.hpp>
+#include <gtl/btree.hpp>
+#include <gtl/vector.hpp>
+#include <gtl/bit_vector.hpp>
+#include <iostream>
+#include <string>
+
+using gtl::flat_hash_map;
+using namespace std::string_literals;
+
+template<class C>
+void check_map() {
+    C c;
+    for (size_t i = 0; i < 100; ++i)
+        c.emplace(std::to_string(i), i);
+    for (size_t i = 1; i < 100; i += 2)
+        c.erase(std::to_string(i));
+    std::cout << c.size() << " elements\n";
+}
+
+template<class C>
+void check_set() {
+   C c;
+    for (size_t i = 0; i < 100; ++i)
+        c.emplace(i);
+    for (size_t i = 1; i < 100; i += 2)
+        c.erase(i);
+    std::cout << c.size() << " elements\n";
+}
+
+void check_vector() {
+   gtl::vector<int> c;
+    for (size_t i = 0; i < 200; ++i)
+        c.emplace_back(i);
+    for (size_t i = 1; i < 100; i++)
+       c.erase(c.begin() + 1);
+    std::cout << c.size() << " elements\n";
+}
+
+
+
+int main() {
+    // flat and node containers
+   // -------------------------
+    check_map<gtl::flat_hash_map<std::string, int>>();
+    check_set<gtl::flat_hash_set<int>>();
+    check_set<gtl::node_hash_set<int>>();
+
+    // parallel flat and node containers
+    // ---------------------------------
+    check_map<gtl::parallel_flat_hash_map<std::string, int>>();
+    check_map<gtl::parallel_node_hash_map<std::string, int>>();
+    check_set<gtl::parallel_flat_hash_set<int>>();
+    check_set<gtl::parallel_node_hash_set<int>>();
+
+    // btree containers
+    // ----------------
+    check_map<gtl::btree_map<std::string, int>>();
+    check_map<gtl::btree_multimap<std::string, int>>();
+    check_set<gtl::btree_set<int>>();
+    check_set<gtl::btree_multiset<int>>();
+
+    // vector
+    // ------
+    check_vector();
+
+    // bit vector
+    // ----------
+    gtl::bit_vector bv{ 0x04321 };
+    [[maybe_unused]] auto v = bv.view(4, 20);
+    return 0;
+}
